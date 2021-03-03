6df02f45fa88989f992a4c48e7ea47bc53542301
Michael Frieze <michaelfrieze@icloud.com>
2021-03-03 18:38:52 -0500

    finished setting up redux with typescrip

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,21 +1,10 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
 interface RepositoriesState {
-  loading: boolean
-  error: string | null
-  data: string[]
-}
-
-interface SearchRepositoriesAction {
-  type: 'search_repositories'
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: 'search_repositories_success'
-  payload: string[]
-}
-
-interface SearchRepositoriesErrorAction {
-  type: 'search_repositories_error'
-  payload: string
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
 const reducer = (
@@ -23,15 +12,15 @@ const reducer = (
   action: Action
 ): RepositoriesState => {
   switch (action.type) {
-    case 'search_repositories':
-      return { loading: true, error: null, data: [] }
-    case 'search_repositories_success':
-      return { loading: false, error: null, data: action.payload }
-    case 'search_repositories_error':
-      return { loading: false, error: action.payload, data: [] }
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default reducer
+export default reducer;
